c648491f75ec7c7af31d942f51e40f2742cd00d6
CandelaSienra <cs326092@fi365.ort.edu.uy>
2025-05-10 16:22:11 -0300

    Creación del proyecto WebApi, con el metodo GetByTracking

---
+++ Obligatorio.LogicaAccesoDatos/AccesoDatos/Excepciones/BadRequestException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio.Infraestructura.AccesoDatos.Excepciones
+{
+    public class BadRequestException : InfraestructuraException
+    {
+        public BadRequestException()
+        {
+        }
+
+        public BadRequestException(string? message) : base(message)
+        {
+        }
+
+        protected BadRequestException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public override int statusCode()
+        {
+            return 400;
+        }
+    }
+}
---
+++ Obligatorio.WebApi/Controllers/EnviosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Obligatorio.CasoDeUsoCompartida.DTOs.Envios;
+using Obligatorio.CasoDeUsoCompartida.InterfacesCU.Envio;
+using Obligatorio.Infraestructura.AccesoDatos.Excepciones;
+
+namespace Obligatorio.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   
+        public class EnviosController : ControllerBase
+        {
+            IGetByTracking<EnvioListadoDto> _getByTracking;
+            
+
+            public EnviosController(IGetByTracking<EnvioListadoDto> getByTracking)
+            {
+                _getByTracking = getByTracking;
+            }
+
+
+            [HttpGet("{tracking}")]
+            public IActionResult GetByTracking(int tracking)
+            {
+                try
+                {
+                    return Ok(_getByTracking.Execute(tracking));
+                }
+                catch (NotFoundException e)
+                {
+                    return StatusCode(404, $"No se encontrò el id {tracking}");
+                }
+                catch (Exception e)
+                {
+                    return StatusCode(500, "Hubo un problema intente nuevamente.");
+                }
+
+            }
+
+         }
+}
