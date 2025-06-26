120fd3d390abb49c8292282159eb7503056d3ddd
kolobits <camilopardo1994@gmail.com>
2025-06-25 22:27:24 -0300

    Finalizo RF4 y correcciones en Login

--- AppCliente/AppCliente/AppCliente/Models/Usuario/UsuarioLoginDto.cs
+++ AppCliente/AppCliente/AppCliente/Models/Usuario/UsuarioLoginDto.cs
@@ -1,8 +1,19 @@
-﻿namespace AppCliente.Models.Usuario
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppCliente.Models.Usuario
 {
-	public record UsuarioLoginDto(
-		string Email,
-		string Password)
+	public record UsuarioLoginDto
+	//(
+	//string Email,
+	//string Password)
 	{
+		[Required(ErrorMessage = "El Email del usuario es requerido")]
+		[EmailAddress(ErrorMessage = "Formato de Email es inválido")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "La contraseña del usuario es requerida")]
+		public string Password { get; set; }
 	}
+
+
 }
